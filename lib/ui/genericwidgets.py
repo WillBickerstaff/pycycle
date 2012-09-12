3e1648361bf6f946fb9b66cbd7b811f71bd8ec50
Will Bickerstaff <will.bickerstaff@gmail.com>
2012-09-12 21:47:19 +0100

    Move LabeledSpin into genericwidgets

---
+++ lib/ui/genericwidgets.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+# Copyright 2010 Will Bickerstaff
+# This file is part of PyCycle.
+#
+# PyCycle is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.read value
+#
+# PyCycle is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# PyCycle. If not, see http://www.gnu.org/licenses/
+
+from Tkinter import Frame, Label, Spinbox, E, W, IntVar
+
+
+class LabeledSpin(Frame):
+    def __init__(self, labeltext='', master=None):
+        Frame.__init__(self, master)
+        self.val = IntVar()
+        self.lbl = Label(text=labeltext)
+        self.lbl.grid(row=0, column=0, sticky=E, in_=self)
+        self.Spin = Spinbox(textvariable=self.val)
+        self.Spin.grid(row=0, column=1, sticky=W, in_=self)
